1f5d3aaea72ad4091b98ea974f17e5c133d31456
Evelio Sosa <Evelio.Sosa@ibm.com>
2021-05-24 14:35:50 -0500

    "update requirements"

--- setup.py
+++ setup.py
@@ -8,11 +8,9 @@ setup(
     url='https://github.com/multicloud-ops/operator-csv-libs',
     packages=['operator_csv_libs'],
     install_requires=[
-        'pyyaml',
-        'pygithub',
-        'dohq-artifactory',
-        'slack_log_handler',
-        'requests',
-        'slackclient'
+        'pyyaml==5.4.1',
+        'pygithub==1.54.1',
+        'dohq-artifactory==0.7.574 ',
+        'requests==2.25.1'
     ]
 )
