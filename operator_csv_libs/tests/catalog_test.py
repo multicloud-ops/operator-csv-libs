6f2d40265d2396ac320baec2cc23e0e7c770a444
Ethan Balcik <ethanbalcik@ibm.com>
2022-10-20 10:22:34 -0400

    Create reference for test channel with 0.6.1 bundle entry

--- operator_csv_libs/tests/catalog_test.py
+++ operator_csv_libs/tests/catalog_test.py
@@ -27,6 +27,15 @@ TEST_CHANNEL = {
     "entries": []
 }
 
+TEST_CHANNEL_WITH_061 = {
+    "schema": "olm.channel",
+    "name": "testChannel",
+    "package": "etcd",
+    "entries": [{
+        "name": "etcdoperator-community.v0.6.1"
+    }]
+}
+
 TEST_CHANNEL_ENTRY = {
     "name": "etcdoperator-community.v100",
     "replaces": "etcdoperator-community.v0.9.4",
@@ -165,10 +174,10 @@ class TestCatalog(unittest.TestCase):
     def test_remove_channel(self):
         self.catalog.add_channel('testChannel', 'etcd')
         self.catalog.add_channel_entry(channel='testChannel', name='etcdoperator-community.v0.6.1')
-        self.assertIn(TEST_CHANNEL, self.catalog.get_channels())
+        self.assertIn(TEST_CHANNEL_WITH_061, self.catalog.get_channels())
         self.assertIn(BUNDLE_061, self.catalog.get_bundles())
         self.catalog.remove_channel('testChannel')
-        self.assertNotIn(TEST_CHANNEL, self.catalog.get_channels())
+        self.assertNotIn(TEST_CHANNEL_WITH_061, self.catalog.get_channels())
         self.assertIn(BUNDLE_061, self.catalog.get_bundles())
 
     def test_remove_bundle(self):
